5bce2ba16ff32649d351fd9920644d89fb83d1e9
filehelper <file.helper@outlook.com>
2022-07-31 21:32:37 +0800

    lab2 lru_replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,42 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) : size_{num_pages} {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool {
+  if (this->size_ <= 0) {
+    return false;
+  }
+  *frame_id = lru_list_.back();
+  lru_list_.pop_back();
+  lru_map_.erase(*frame_id);
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  auto iter = lru_map_.find(frame_id);
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+  if (iter != lru_map_.end()) {
+    lru_list_.erase(lru_map_[frame_id]);
+    lru_map_.erase(frame_id);
+  }
+}
 
-auto LRUReplacer::Size() -> size_t { return 0; }
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  auto iter = lru_map_.find(frame_id);
+  if (iter == lru_map_.end()) {
+    lru_list_.push_front(frame_id);
+    lru_map_[frame_id] = lru_list_.begin();
+    if (lru_map_.size() > this->size_) {
+      frame_id_t ret = lru_list_.back();
+      lru_list_.pop_back();
+      lru_map_.erase(ret);
+    }
+  }
+}
+
+auto LRUReplacer::Size() -> size_t { return lru_map_.size(); }
 
 }  // namespace bustub
